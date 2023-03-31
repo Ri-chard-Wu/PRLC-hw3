1e798c7fdf554c80f346d2d4a0122238101aba95
Ri-chard-Wu <glotigorgeous@gmail.com>
2023-03-31 08:33:06 +0000

    backup, completed sobel() kernel. Not tested.

--- hw3.cu
+++ hw3.cu
@@ -6,50 +6,48 @@
 #include <cstdlib>
 #include <iostream>
 
-#define MASK_N 2
-#define MASK_X 5
-#define MASK_Y 5
-#define SCALE 8
+// #define MASK_N 2
+// #define MASK_X 5
+// #define MASK_Y 5
+// #define SCALE 8
 
 
 
 
-#define CC_GRID_MAX_X_DIM (1 << 31) - 1
-#define CC_GRID_MAX_Y_DIM 65535
-#define CC_GRID_MAX_Z_DIM 65535
+// #define CC_GRID_MAX_X_DIM (1 << 31) - 1
+// #define CC_GRID_MAX_Y_DIM 65535
+// #define CC_GRID_MAX_Z_DIM 65535
 
-#define CC_BLOCK_MAX_X_DIM 1024
-#define CC_BLOCK_MAX_Y_DIM 1024
-#define CC_BLOCK_MAX_Z_DIM 64
+// #define CC_BLOCK_MAX_X_DIM 1024
+// #define CC_BLOCK_MAX_Y_DIM 1024
+// #define CC_BLOCK_MAX_Z_DIM 64
 
-#define CC_BLOCK_MAX_N_THREADS 1024
+// #define CC_BLOCK_MAX_N_THREADS 1024
 
-#define CC_MAX_N_RSD_BLOCKS 32
-#define CC_MAX_N_RSD_WARPS 64
-#define CC_MAX_N_RSD_THREADS 2048
-
-
-// 1.png: 4928 x 3264 x 3
-// 2.png: 16320 x 10809 x 3
-// 3.png: 634 x 634 x 3
-// 4.png: 900 x 622 x 3
-// 5.png: 1800 x 1244 x 3
-// 6.png: 3600 x 2488 x 3
-// 7.png: 7200 x 4976 x 3
-// 8.png: 14400 x 9952 x 3
+// #define CC_MAX_N_RSD_BLOCKS 32
+// #define CC_MAX_N_RSD_WARPS 64
+// #define CC_MAX_N_RSD_THREADS 2048
 
 
-// #define GRID_N_X
-// #define GRID_N_Y
-#define GRID_N_Z 3
+// // 1.png: 4928 x 3264 x 3
+// // 2.png: 16320 x 10809 x 3
+// // 3.png: 634 x 634 x 3
+// // 4.png: 900 x 622 x 3
+// // 5.png: 1800 x 1244 x 3
+// // 6.png: 3600 x 2488 x 3
+// // 7.png: 7200 x 4976 x 3
+// // 8.png: 14400 x 9952 x 3
 
-#define BLOCK_N_X 8
-#define BLOCK_N_Y 8
-#define BLOCK_N_Z 1
 
-// #define BLOCK_N_THREADS
+// // #define GRID_N_X
+// // #define GRID_N_Y
+// #define GRID_N_Z 3
 
+// #define BLOCK_N_X 8
+// #define BLOCK_N_Y 8
+// #define BLOCK_N_Z 1
 
+// // #define BLOCK_N_THREADS
 
 
 
@@ -57,233 +55,201 @@
 
 
 
-// clang-format off
 
-int mask[MASK_N][MASK_X][MASK_Y] = {
+
+// // clang-format off
+
+// int mask[MASK_N][MASK_X][MASK_Y] = {
   
-    {{ -1, -4, -6, -4, -1},
-     { -2, -8,-12, -8, -2},
-     {  0,  0,  0,  0,  0},
-     {  2,  8, 12,  8,  2},
-     {  1,  4,  6,  4,  1}},
+//     {{ -1, -4, -6, -4, -1},
+//      { -2, -8,-12, -8, -2},
+//      {  0,  0,  0,  0,  0},
+//      {  2,  8, 12,  8,  2},
+//      {  1,  4,  6,  4,  1}},
 
-    {{ -1, -2,  0,  2,  1},
-     { -4, -8,  0,  8,  4},
-     { -6,-12,  0, 12,  6},
-     { -4, -8,  0,  8,  4},
-     { -1, -2,  0,  2,  1}}
+//     {{ -1, -2,  0,  2,  1},
+//      { -4, -8,  0,  8,  4},
+//      { -6,-12,  0, 12,  6},
+//      { -4, -8,  0,  8,  4},
+//      { -1, -2,  0,  2,  1}}
 
-};
-
-
+// };
 
 
-int read_png(const char* filename, unsigned char** image, unsigned* height, unsigned* width,
-    unsigned* channels) {
-    unsigned char sig[8];
-    FILE* infile;
-    infile = fopen(filename, "rb");
-
-    fread(sig, 1, 8, infile);
-    if (!png_check_sig(sig, 8)) return 1; /* bad signature */
-
-    png_structp png_ptr;
-    png_infop info_ptr;
-
-    png_ptr = png_create_read_struct(PNG_LIBPNG_VER_STRING, NULL, NULL, NULL);
-    if (!png_ptr) return 4; /* out of memory */
-
-    info_ptr = png_create_info_struct(png_ptr);
-    if (!info_ptr) {
-        png_destroy_read_struct(&png_ptr, NULL, NULL);
-        return 4; /* out of memory */
-    }
-
-    png_init_io(png_ptr, infile);
-    png_set_sig_bytes(png_ptr, 8);
-    png_read_info(png_ptr, info_ptr);
-    int bit_depth, color_type;
-    png_get_IHDR(png_ptr, info_ptr, width, height, &bit_depth, &color_type, NULL, NULL, NULL);
-
-    png_uint_32 i, rowbytes;
-    png_bytep row_pointers[*height];
-    png_read_update_info(png_ptr, info_ptr);
-    rowbytes = png_get_rowbytes(png_ptr, info_ptr);
-    *channels = (int)png_get_channels(png_ptr, info_ptr);
 
 
-    if ((*image = (unsigned char*)calloc((rowbytes + 4*3) * (*height + 4), 1)) == NULL) {
-        png_destroy_read_struct(&png_ptr, &info_ptr, NULL);
-        return 3;
-    }
+// int read_png(const char* filename, unsigned char** image, unsigned* height, unsigned* width,
+//     unsigned* channels) {
+//     unsigned char sig[8];
+//     FILE* infile;
+//     infile = fopen(filename, "rb");
+
+//     fread(sig, 1, 8, infile);
+//     if (!png_check_sig(sig, 8)) return 1; /* bad signature */
+
+//     png_structp png_ptr;
+//     png_infop info_ptr;
+
+//     png_ptr = png_create_read_struct(PNG_LIBPNG_VER_STRING, NULL, NULL, NULL);
+//     if (!png_ptr) return 4; /* out of memory */
+
+//     info_ptr = png_create_info_struct(png_ptr);
+//     if (!info_ptr) {
+//         png_destroy_read_struct(&png_ptr, NULL, NULL);
+//         return 4; /* out of memory */
+//     }
+
+//     png_init_io(png_ptr, infile);
+//     png_set_sig_bytes(png_ptr, 8);
+//     png_read_info(png_ptr, info_ptr);
+//     int bit_depth, color_type;
+//     png_get_IHDR(png_ptr, info_ptr, width, height, &bit_depth, &color_type, NULL, NULL, NULL);
+
+//     png_uint_32 i, rowbytes;
+//     png_bytep row_pointers[*height];
+//     png_read_update_info(png_ptr, info_ptr);
+//     rowbytes = png_get_rowbytes(png_ptr, info_ptr);
+//     *channels = (int)png_get_channels(png_ptr, info_ptr);
+
+
+//     if ((*image = (unsigned char*)calloc((rowbytes + 4*3) * (*height + 4), 1)) == NULL) {
+//         png_destroy_read_struct(&png_ptr, &info_ptr, NULL);
+//         return 3;
+//     }
  
-    for (i = 0; i < *height; ++i) {
-        row_pointers[i] = *image + (i + 2) * (rowbytes + 4*3) + 2*3;
-    }
+//     for (i = 0; i < *height; ++i) {
+//         row_pointers[i] = *image + (i + 2) * (rowbytes + 4*3) + 2*3;
+//     }
 
-    png_read_image(png_ptr, row_pointers);
-    png_read_end(png_ptr, NULL);
-    png_destroy_read_struct(&png_ptr, &info_ptr, NULL);
-    return 0;
+//     png_read_image(png_ptr, row_pointers);
+//     png_read_end(png_ptr, NULL);
+//     png_destroy_read_struct(&png_ptr, &info_ptr, NULL);
+//     return 0;
+// }
+
+
+// void write_png(const char* filename, png_bytep image, const unsigned height, const unsigned width,
+//     const unsigned channels) {
+//     FILE* fp = fopen(filename, "wb");
+//     png_structp png_ptr = png_create_write_struct(PNG_LIBPNG_VER_STRING, NULL, NULL, NULL);
+//     png_infop info_ptr = png_create_info_struct(png_ptr);
+//     png_init_io(png_ptr, fp);
+//     png_set_IHDR(png_ptr, info_ptr, width, height, 8, PNG_COLOR_TYPE_RGB, PNG_INTERLACE_NONE,
+//         PNG_COMPRESSION_TYPE_DEFAULT, PNG_FILTER_TYPE_DEFAULT);
+//     png_set_filter(png_ptr, 0, PNG_NO_FILTERS);
+//     png_write_info(png_ptr, info_ptr);
+//     png_set_compression_level(png_ptr, 0);
+
+//     png_bytep row_ptr[height];
+//     for (int i = 0; i < height; ++i) {
+//         row_ptr[i] = image + i * width * channels * sizeof(unsigned char);
+//     }
+//     png_write_image(png_ptr, row_ptr);
+//     png_write_end(png_ptr, NULL);
+//     png_destroy_write_struct(&png_ptr, &info_ptr);
+//     fclose(fp);
+// }
+
+
+
+// __global__ void sobel(unsigned char *s, unsigned char *t, 
+//                                 unsigned height, unsigned width, unsigned channels)
+// {
+//     int basex = blockIdx.x * blockDim.x;
+//     int basey = blockIdx.y * blockDim.y;
+    
+//     int nextBasex = (blockIdx.x + 1) * blockDim.x;
+//     int nextBasey = (blockIdx.y + 1) * blockDim.y;
+
+//     int x = blockIdx.x * blockDim.x + threadIdx.x;
+//     int y = blockIdx.y * blockDim.y + threadIdx.y;
+//     int z = blockIdx.z * blockDim.z + threadIdx.z;
+
+//     if(x >= width || y >= height) return;
+
+//     __shared__ unsigned char smSrc[(BLOCK_N_X + 4) * (BLOCK_N_Y + 4)];
+
+//     smSrc[BLOCK_N_X * threadIdx.y + threadIdx.x] = s[3 * ((width + 4) * y + x) + z];
+    
+//     if(threadIdx.x < 4){
+//         smSrc[BLOCK_N_X * threadIdx.y + threadIdx.x + BLOCK_N_X] =\
+//                     s[3 * ((width + 4) * (basey + threadIdx.y) + nextBasex + threadIdx.x) + z];
+//     }
+
+//     if(threadIdx.y < 4){
+//         smSrc[BLOCK_N_X * (BLOCK_N_Y + threadIdx.y) + threadIdx.x] =\
+//                     s[3 * ((width + 4) * (nextBasey + threadIdx.y) + basex + threadIdx.x) + z];
+//     }
+
+//     if(threadIdx.x < 4 && threadIdx.y < 4){
+//         smSrc[BLOCK_N_X * (BLOCK_N_Y + threadIdx.y) + BLOCK_N_X + threadIdx.x] = \
+//                     s[3 * ((width + 4) * (nextBasey + threadIdx.y) + nextBasex + threadIdx.x) + z];
+//     }
+
+//     __syncthreads();
+
+//     double val[MASK_N] = {0.0};
+    
+//     for (i = 0; i < MASK_N; ++i){
+//         for (v = 0; v < 5; ++v){
+//             for (u = 0; u < 5; ++u){
+//                 val[i] += smSrc[BLOCK_N_X * (threadIdx.y + v) + threadIdx.x + u] * mask[i][u][v];
+//             }
+//         }
+//     }
+
+//     val[0] = sqrt(val[0] * val[0] + val[1] * val[1]) / SCALE;
+//     t[3 * (width * y + x) + z] = (val[0] > 255.0) ? 255 : val[0];
+    
+// }
+
+
+
+// int main(int argc, char** argv) {
+//     assert(argc == 3);
+    
+    
+//     unsigned height, width, channels, gridNx, gridNy;
+//     unsigned char *src_img = NULL;
+//     unsigned char* dst_img =
+//         (unsigned char*) malloc(height * width * channels * sizeof(unsigned char));
+
+//     read_png(argv[1], &src_img, &height, &width, &channels);
+//     assert(channels == 3);
+//     printf("width x height: %d x %d\n", width, height);
+//     cc_check_param(width, height);
+
+//     gridNx = width / BLOCK_N_X + 1;
+//     gridNy = height / BLOCK_N_Y + 1;
+//     dim3 nThreadsPerBlock(BLOCK_N_X, BLOCK_N_Y, BLOCK_N_Z);
+//     dim3 nBlocks(gridNx, gridNy, GRID_N_Z);
+
+//     unsigned char *devSrc, *devDst;
+//     cudaMalloc(&devSrc, (height + 4) * (width + 4) * channels * sizeof(unsigned char));
+//     cudaMalloc(&devDst, height * width * channels * sizeof(unsigned char));
+//     cudaMemcpy(devSrc, src_img, height * width * channels * sizeof(unsigned char), cudaMemcpyHostToDevice);
+    
+//     sobel<<<nBlocks, nThreadsPerBlock>>>(devSrc, devDst, height, width, channels); 
+
+//     cudaMemcpy(dst_img, devDst, height * width * channels * sizeof(unsigned char), cudaMemcpyDeviceToHost);
+
+//     write_png(argv[2], dst_img, height, width, channels);
+
+//     cudaFree(devSrc);
+//     cudaFree(devDst);
+//     free(src_img);
+//     free(dst_img);
+
+//     return 0;
+// }
+
+
+__global__ void cuda_hello(){
+    printf("Hello World from GPU!\n");
 }
 
-
-void write_png(const char* filename, png_bytep image, const unsigned height, const unsigned width,
-    const unsigned channels) {
-    FILE* fp = fopen(filename, "wb");
-    png_structp png_ptr = png_create_write_struct(PNG_LIBPNG_VER_STRING, NULL, NULL, NULL);
-    png_infop info_ptr = png_create_info_struct(png_ptr);
-    png_init_io(png_ptr, fp);
-    png_set_IHDR(png_ptr, info_ptr, width, height, 8, PNG_COLOR_TYPE_RGB, PNG_INTERLACE_NONE,
-        PNG_COMPRESSION_TYPE_DEFAULT, PNG_FILTER_TYPE_DEFAULT);
-    png_set_filter(png_ptr, 0, PNG_NO_FILTERS);
-    png_write_info(png_ptr, info_ptr);
-    png_set_compression_level(png_ptr, 0);
-
-    png_bytep row_ptr[height];
-    for (int i = 0; i < height; ++i) {
-        row_ptr[i] = image + i * width * channels * sizeof(unsigned char);
-    }
-    png_write_image(png_ptr, row_ptr);
-    png_write_end(png_ptr, NULL);
-    png_destroy_write_struct(&png_ptr, &info_ptr);
-    fclose(fp);
-}
-
-
-
-void sobel(unsigned char* s, unsigned char* t, unsigned height, unsigned width, unsigned channels) {
-  
-    int x, y, i, v, u;
-    int R, G, B;
-    double val[MASK_N * 3] = {0.0};
-    
-    int adjustX, adjustY, xBound, yBound;
-    adjustX = (MASK_X % 2) ? 1 : 0;
-    adjustY = (MASK_Y % 2) ? 1 : 0;
-    xBound = MASK_X / 2;
-    yBound = MASK_Y / 2;
-
-    for (y = 0; y < height; ++y) {
-
-        for (x = 0; x < width; ++x) {
-
-            for (i = 0; i < MASK_N; ++i) {
-
-                val[i * 3 + 2] = 0.0;
-                val[i * 3 + 1] = 0.0;
-                val[i * 3] = 0.0;
-
-                for (v = -yBound; v < yBound + adjustY; ++v) {
-                    for (u = -xBound; u < xBound + adjustX; ++u) {
-                        if ((x + u) >= 0 && (x + u) < width && y + v >= 0 && y + v < height) {
-                            R = s[channels * (width * (y + v) + (x + u)) + 2];
-                            G = s[channels * (width * (y + v) + (x + u)) + 1];
-                            B = s[channels * (width * (y + v) + (x + u)) + 0];
-                            val[i * 3 + 2] += R * mask[i][u + xBound][v + yBound];
-                            val[i * 3 + 1] += G * mask[i][u + xBound][v + yBound];
-                            val[i * 3 + 0] += B * mask[i][u + xBound][v + yBound];
-                        }
-                    }
-                }
-            }
-
-            double totalR = 0.0;
-            double totalG = 0.0;
-            double totalB = 0.0;
-            for (i = 0; i < MASK_N; ++i) {
-                totalR += val[i * 3 + 2] * val[i * 3 + 2];
-                totalG += val[i * 3 + 1] * val[i * 3 + 1];
-                totalB += val[i * 3 + 0] * val[i * 3 + 0];
-            }
-
-            totalR = sqrt(totalR) / SCALE;
-            totalG = sqrt(totalG) / SCALE;
-            totalB = sqrt(totalB) / SCALE;
-            const unsigned char cR = (totalR > 255.0) ? 255 : totalR;
-            const unsigned char cG = (totalG > 255.0) ? 255 : totalG;
-            const unsigned char cB = (totalB > 255.0) ? 255 : totalB;
-            t[channels * (width * y + x) + 2] = cR;    
-            t[channels * (width * y + x) + 1] = cG;    
-            t[channels * (width * y + x) + 0] = cB;      
-        }   
-    }
-}
-
-
-
-__global__ void sobel(unsigned char *s, unsigned char *t, unsigned height, unsigned width, unsigned channels)
-{
-    int basex = blockIdx.x * blockDim.x;
-    int basey = blockIdx.y * blockDim.y;
-    
-    int nextBasex = (blockIdx.x + 1) * blockDim.x;
-    int nextBasey = (blockIdx.y + 1) * blockDim.y;
-
-    int x = blockIdx.x * blockDim.x + threadIdx.x;
-    int y = blockIdx.y * blockDim.y + threadIdx.y;
-    int z = blockIdx.z * blockDim.z + threadIdx.z;
-
-    __shared__ unsigned char smSrc[(BLOCK_N_X + 4) * (BLOCK_N_Y + 4)];
-
-    smSrc[BLOCK_N_X * threadIdx.y + threadIdx.x] = s[3 * ((width + 4) * y + x) + z];
-    
-    if(threadIdx.x < 4){
-        smSrc[BLOCK_N_X * threadIdx.y + threadIdx.x + BLOCK_N_X] =\
-                     s[3 * ((width + 4) * (basey + threadIdx.y) + nextBasex + threadIdx.x) + z];
-    }
-
-    if(threadIdx.y < 4){
-        smSrc[BLOCK_N_X * (BLOCK_N_Y + threadIdx.y) + threadIdx.x] =\
-                     s[3 * ((width + 4) * (nextBasey + threadIdx.y) + basex + threadIdx.x) + z];
-    }
-
-    if(threadIdx.x < 4 && threadIdx.y < 4){
-        smSrc[BLOCK_N_X * (BLOCK_N_Y + threadIdx.y) + BLOCK_N_X + threadIdx.x] = \
-                        s[3 * ((width + 4) * (nextBasey + threadIdx.y) + nextBasex + threadIdx.x) + z];
-    }
-
-    __syncthreads();
-
-}
-
-
-
-int main(int argc, char** argv) {
-    assert(argc == 3);
-    
-    
-    unsigned height, width, channels, gridNx, gridNy;
-    unsigned char *src_img = NULL;
-    unsigned char* dst_img =
-        (unsigned char*) malloc(height * width * channels * sizeof(unsigned char));
-
-    read_png(argv[1], &src_img, &height, &width, &channels);
-    assert(channels == 3);
-    printf("width x height: %d x %d\n", width, height);
-    cc_check_param(width, height);
-
-    gridNx = width / BLOCK_N_X + 1;
-    gridNy = height / BLOCK_N_Y + 1;
-    dim3 nThreadsPerBlock(BLOCK_N_X, BLOCK_N_Y, BLOCK_N_Z);
-    dim3 nBlocks(gridNx, gridNy, GRID_N_Z);
-
-    unsigned char *devSrc, *devDst;
-    cudaMalloc(&devSrc, (height + 4) * (width + 4) * channels * sizeof(unsigned char));
-    cudaMalloc(&devDst, height * width * channels * sizeof(unsigned char));
-    cudaMemcpy(devSrc, src_img, height * width * channels * sizeof(unsigned char), cudaMemcpyHostToDevice);
-    
-    sobel<<<nBlocks, nThreadsPerBlock>>>(devSrc, devDst, height, width, channels); 
-
-    cudaMemcpy(dst_img, devDst, height * width * channels * sizeof(unsigned char), cudaMemcpyDeviceToHost);
-
-
-    write_png(argv[2], dst_img, height, width, channels);
-
-    cudaFree(devSrc);
-    cudaFree(devDst);
-    free(src_img);
-    free(dst_img);
-
+int main() {
+    cuda_hello<<<1,1>>>(); 
+    cudaDeviceSynchronize();
     return 0;
 }
---
+++ print_from_kernel.cu
@@ -0,0 +1,19 @@
+
+#include <png.h>
+#include <zlib.h>
+
+#include <cassert>
+#include <cmath>
+#include <cstdlib>
+#include <iostream>
+
+
+__global__ void cuda_hello(){
+    printf("Hello World from GPU!\n");
+}
+
+int main() {
+    cuda_hello<<<1,1>>>(); 
+    cudaDeviceSynchronize();
+    return 0;
+}
